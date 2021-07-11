f79305f921d23c122242fb3ffa6360a53fd4e460
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-07-11 16:13:56 +0300

    feat: simple access token authentication

---
+++ lib/my_app_web/controllers/account_controller.ex
@@ -0,0 +1,46 @@
+defmodule MyAppWeb.AccountController do
+  use MyAppWeb, :controller
+
+  alias MyApp.Identity
+  alias MyApp.Identity.{Guardian}
+
+  action_fallback MyAppWeb.FallbackController
+
+  def register(conn, %{"account" => account_params}) do
+    case Identity.create_account(account_params) do
+      {:ok, account} ->
+        conn
+        |> account_with_token(account)
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  def login(conn, %{"email" => email, "password" => password}) do
+    case Identity.authenticate_account(email, password) do
+      {:ok, account} ->
+        conn
+        |> account_with_token(account)
+      error ->
+        error
+    end
+  end
+
+  def logout(conn, _params) do
+    # Currently does nothing, will be needed to add GuardianDB
+    conn
+    |> render("logout.json")
+  end
+
+  def me(conn, _params) do
+    account = Guardian.Plug.current_resource(conn)
+    conn
+    |> render("account.json", account: account)
+  end
+
+  defp account_with_token(conn, account) do
+    {:ok, token} = Identity.generate_token(account)
+    conn
+    |> render("account.token.json", account: account, token: token)
+  end
+end
