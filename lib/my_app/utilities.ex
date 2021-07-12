5859558eea256a6b9c1209ed3c52ebdb12c4aa06
Mohammed Ali Agha <61318540+dodiameer@users.noreply.github.com>
2021-07-12 15:44:52 +0300

    fix: Return nil if passed value is also nil

--- lib/my_app/utilities.ex
+++ lib/my_app/utilities.ex
@@ -1,6 +1,7 @@
 defmodule MyApp.Utilities do
 
   def decode_signed_cookie(""), do: nil
+  def decode_signed_cookie(nil), do: nil
   def decode_signed_cookie(cookie) do
     [_, payload, _] = String.split(cookie, ".", parts: 3)
     {:ok, encoded_term } = Base.url_decode64(payload, padding: false)
